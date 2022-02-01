f2f3cb2dbc3637d6b33a4d08324212a38c3ac86e
Joshuel <joshuel.simbulan@gmail.com>
2022-02-01 20:54:58 +0800

    Binary Tree 0

--- binarytree.c
+++ binarytree.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern int x;
+extern int y;
+
 typedef struct binarytree
 {
     int f;
     char ch;
     struct binarytree *left, *right;
 }binarytree;
+
+void assign(void)
+{
+    x = 1;
+    y = 2;
+}
+
+binarytree *createTreeNode(int f, char ch)
+{
+    binarytree *newNode;
+    newNode = malloc(sizeof(binarytree));
+    newNode->f = f;
+    newNode->ch = ch;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
 
--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct queuenode
+{
+    int value; // int muna ngayon pero later magiging treenode na yan
+    struct queuenode *next;
+}queuenode;
+
+void enqueue(queuenode **curr, int value);
+
+//int x;
+//int y;
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    //x = 100;
+    //y = 200;
+    //assign();
+    queuenode *front = NULL;
+
+    enqueue(&front, 0);
+    enqueue(&front, 5);
+    enqueue(&front, 2);
+
     //for (queuenode *curr = front; curr != NULL; curr = curr->next)
     //{
     //    printf("Value: %d\n", curr->value);
     //}
+
+    printf("%d, %d", x, y);
 }
